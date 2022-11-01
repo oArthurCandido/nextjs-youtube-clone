a8e9d64c82ee67437e42e338c1b837ac996b5bc7
Arthur Candido <oarthurcandido@gmail.com>
2022-11-01 18:45:39 -0300

    :wrench:chore: config mongodb and multer

---
+++ src/pages/api/video.js
@@ -0,0 +1,29 @@
+import nc from 'next-connect';
+import upload from 'src/utils/upload';
+
+const handler = nc()
+  .use(upload.single('file'))
+  .post((req, res) => {
+    const { title, authorName, authorAvatar, videoUrl } = req.body;
+    res.json({ hello: 'world' });
+  })
+  .patch(async (req, res) => {
+    throw new Error('Throws me around! Error can be caught and handled.');
+  });
+
+export default handler;
+
+// export default function userHandler(req, res) {
+//   const { method } = req;
+
+//   switch (method) {
+//     case 'POST':
+//       // Get data from your database
+//       res.status(200).json({ msg: ` ${method}` });
+//       break;
+
+//     default:
+//       res.setHeader('Allow', ['GET', 'PUT']);
+//       res.status(405).end(`Method ${method} Not Allowed`);
+//   }
+// }
---
+++ src/utils/uploads.js
@@ -0,0 +1,32 @@
+import aws from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, fileName);
+      });
+    }
+  })
+});
+
+export default upload;
