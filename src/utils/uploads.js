a0d8a22bf9db083c840e183981e0dea60452218a
Arthur Candido <oarthurcandido@gmail.com>
2022-11-02 15:10:43 -0300

    :wrench:chore: changing env variable names to permit vercel use

--- src/utils/uploads.js
+++ src/utils/uploads.js
@@ -4,15 +4,15 @@ import multerS3 from 'multer-s3';
 import crypto from 'crypto';
 
 const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_ART,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ART,
+  region: process.env.AWS_REGION_ART
 });
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.AWS_BUCKET_NAME,
+    bucket: process.env.AWS_BUCKET_NAME_ART,
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
